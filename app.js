6ee6faf2bfb4b931af35595d00ae8e68791d9d7b
Hiroshi Isobe <isobe69b@gmail.com>
2022-04-07 11:05:35 -0700

    Fix: recreate the project without create-react-app

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+let mongoDB = process.env.MONGO_URL;
+
+mongoose.connect(mongoDB).then(() => {
+  console.log("connected");
+  const server = app.listen(8080, () => console.log("listening"));
+});
