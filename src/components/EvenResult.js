e8cbf1b5a584ce9666601a6a8a6a87210ba7f714
Hiroshi Isobe <isobe69b@gmail.com>
2022-04-10 16:24:57 -0700

    Build: Delay the rendering of even result component

--- src/components/EvenResult.js
+++ src/components/EvenResult.js
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+const useDelay = (msec) => {
+  const [waiting, setWaiting] = useState(true);
+  useEffect(() => {
+    setTimeout(() => setWaiting(false), msec);
+  }, []);
+  return waiting;
+};
+
 const EvenResult = (props) => {
   const costPerPerson = props.totalAmountOfBill / props.participants.length;
+  const waiting = useDelay(1500);
 
   return (
-    <div className="even-result">
-      <h2>Result(Even)</h2>
-      <p>Everyone Will Pay ${costPerPerson}.</p>
-      {/* saveResultAndGoBack() is declared in CreationPage.js */}
-      <button onClick={(event) => props.saveResultAndGoBack(event, "everyone")}>
-        Save Result and Go Back
-      </button>
-      <button onClick={(event) => props.dontSaveResultAndGoBack(event)}>
-        Don't Save and Go Back
-      </button>
-    </div>
+    <>
+      {waiting ? (
+        <div className="even-result">
+          <p>Calculating...</p>
+        </div>
+      ) : (
+        <div className="even-result">
+          <h2>Result(Even)</h2>
+          <p>Everyone Will Pay ${costPerPerson}.</p>
+          {/* saveResultAndGoBack() is declared in CreationPage.js */}
+          <button
+            onClick={(event) => props.saveResultAndGoBack(event, "everyone")}
+          >
+            Save Result and Go Back
+          </button>
+          <button onClick={(event) => props.dontSaveResultAndGoBack(event)}>
+            Don't Save and Go Back
+          </button>
+        </div>
+      )}
+    </>
   );
 };
 
