03c3dd47f720081f06c07e193c45f89b40d41f57
Hiroshi Isobe <isobe69b@gmail.com>
2022-04-10 19:06:27 -0700

    Build: Fix spelling mistake

---
+++ src/components/DetailModal.js
@@ -0,0 +1,39 @@
+import React from "react";
+const DetailModal = (props) => {
+  const detailHistory = props.historyArr.find(
+    (history) => history._id === props.whichHistory
+  );
+
+  return (
+    <div id="history-detail">
+      <h3>Detail</h3>
+      <section id="detailSection">
+        <p>Date: {detailHistory.date.substr(0, 10)}</p>
+        <p>Title: {detailHistory.title}</p>
+        <p>Total Bill: ${detailHistory.totalAmountOfBill}</p>
+        <p>Split Type: {detailHistory.splitType}</p>
+        <p>Payer: {detailHistory.payer}</p>
+        <div>
+          Participants:
+          <ul>
+            {detailHistory.participants.map((participant) => (
+              <li key={participant}>{participant}</li>
+            ))}
+          </ul>
+        </div>
+        <p>Description: {detailHistory.description}</p>
+      </section>
+
+      <button
+        onClick={(event) => {
+          props.setShowDetail(!props.showDetail);
+          props.setWhichHistory("");
+        }}
+      >
+        Close
+      </button>
+    </div>
+  );
+};
+
+export default DetailModal;
